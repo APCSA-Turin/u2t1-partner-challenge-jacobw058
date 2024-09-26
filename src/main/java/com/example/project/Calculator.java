25f91ee939a8f691e80e94e494d1b057f060c73b
jacobw058 <jacobw96@nycstudents.net>
2024-09-26 15:38:41 +0000

    Add code

--- src/main/java/com/example/project/Calculator.java
+++ src/main/java/com/example/project/Calculator.java
@@ -3,8 +3,8 @@ public class Calculator{
     public String calcModel;
     
     //constructor that creates an instance of the calculator class
-    public Calculator() {
-        //implement code here
+    public Calculator(String model) {
+        calcModel = model;
     }
     
     // method that performs the operation designated by operand (which
@@ -15,30 +15,43 @@ public class Calculator{
     // REMINDER: use .equals to test if one string is equal to another (not ==)
     // (THIS METHOD DOES NOT PRINT)
     public double performOperation(String operand, int num1, int num2) {
-        //implement code here
-        return 0.00;
+        double double1 = num1;
+        double double2 = num2;
+        if (operand.equals("+")) {
+            return double1 + double2;
+        } else if (operand.equals("-")) {
+            return double1 - double2;
+        } else if (operand.equals("*")) {
+            return double1 * double2;
+        } else if (operand.equals("/")) {
+            return double1 / double2;
+        } else {
+            return 0;
+        }
     }
 
     // method that checks if num1 is evenly divisible by num2, and if so,
     // returns true, and if not, returns false
     // (THIS METHOD DOES NOT PRINT)
     public boolean divisibleBy(int num1, int num2) {
-        //implement code here
-        return false;
+        if((num1 % num2) == (0)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // method that constructs and returns a string representing a coordinate
     // pair in the format: "(x, y)"
     // (THIS METHOD DOES NOT PRINT)
     public String coordinatePair(int x, int y) {
-        //implement code here
-        return "";
+        return "(" + x + "," + y + (")");
     }
     // method that determines and returns the result of |num1 - num2|
     // (THIS METHOD DOES NOT PRINT)
     public int absoluteValue(int num1, int num2) {
-        //implement code here
-        return 0;
+        // https://www.w3schools.com/java/ref_math_abs.asp
+        return Math.abs(num1 - num2);
     }
     
     // method that constructs and returns a String in the format:
@@ -46,7 +59,6 @@ public class Calculator{
     // with the value stored in the calcModel instance variable."
     // (THIS METHOD DOES NOT PRINT)
     public String info() {
-        //implement code here
-        return "";
+        return "This calculator is a model " + calcModel + " where " + calcModel + " should be filled in with the value stored in the calcModel instance variable.";
     }
 }
--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -1,12 +1,69 @@
 package com.example.project;
 
+import java.util.Scanner;
+
 public class CalculatorRunner {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the model of the calculator:\n> ");
+        String calcModel = scanner.nextLine();
+        Calculator calculator = new Calculator(calcModel);
 
-        //test your program here
-        //1. create an instance of the calculator class
+        int menuOption = -1;
+        while (menuOption != 0) {
+            System.out.println(calcModel);
+            System.out.println(" [1] Perform basic operation");
+            System.out.println(" [2] Check divisiblity");
+            System.out.println(" [3] Print coordinate pair");
+            System.out.println(" [4] Get absolute value of difference");
+            System.out.println(" [5] Get calculator information");
+            System.out.println(" [0] Exit");
+            System.out.print("\nEnter a number:\n> ");
+            menuOption = scanner.nextInt();
+            scanner.nextLine();
 
-        //2. call any methods of that class to test
-
+            if (menuOption == 1) {
+                System.out.print("Enter the first integer: ");
+                int int1 = scanner.nextInt();
+                System.out.print("Enter the second integer: ");
+                int int2 = scanner.nextInt();
+                scanner.nextLine();
+                System.out.print("Enter the operation symbol (+, -, *, /):\n> ");
+                String operation = scanner.nextLine();
+                double result = calculator.performOperation(operation, int1, int2);
+                System.out.println("Result of " + int1 + " " + operation + " " + int2 + ": " + result);
+            } else if (menuOption == 2) {
+                System.out.print("Enter the dividend: ");
+                int div1 = scanner.nextInt();
+                System.out.print("Enter the divisor: ");
+                int div2 = scanner.nextInt();
+                scanner.nextLine();
+                boolean isDivisible = calculator.divisibleBy(div1, div2);
+                if (isDivisible) {
+                    System.out.println(div1 + " is divisible by " + div2);
+                } else {
+                    System.out.println(div1 + " is not divisible by " + div2);
+                }
+            } else if (menuOption == 3) {
+                System.out.print("Enter the x value: ");
+                int x = scanner.nextInt();
+                System.out.print("\nEnter the y value: ");
+                int y = scanner.nextInt();
+                System.out.println(calculator.coordinatePair(x, y));
+            } else if (menuOption == 4) {
+                System.out.print("Enter the first integer: ");
+                int int1 = scanner.nextInt();
+                System.out.print("Enter the second integer: ");
+                int int2 = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("The result is " + calculator.absoluteValue(int1, int2));
+            } else if (menuOption == 5) {
+                System.out.println(calculator.info());
+            } else {
+                continue;
+            }
+            System.out.print("Press enter to continue");
+            scanner.nextLine();
+        }
     }
 }
